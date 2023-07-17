bcb65080e3b658fc8acf4160d68b1690278fd527
Andreas Paul <xorpaul@gmail.com>
2023-07-17 16:38:15 +0200

    check version >= 1.20 because LFS_CONTENT_PATH is deprecated

--- manifests/config.pp
+++ manifests/config.pp
@@ -62,7 +62,7 @@ class gitea::config (
 
   $cmp_version = versioncmp($gitea::version, '1.20', true)
   notify { "comparing ${gitea::version} with '1.20' results in ${cmp_version}": }
-  if $cmp_version <= 0 {
+  if $cmp_version < 0 {
     $required_settings = {
       '' => {
         'RUN_USER' => $owner,
